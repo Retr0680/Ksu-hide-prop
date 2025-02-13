ea8e9927ec202868682105475d98af78e3798739
Retr0680 <dedsecretr0680@gmail.com>
2025-02-13 12:33:14 +0530

    fixed a few problems

--- boot-completed.sh
+++ boot-completed.sh
@@ -1,4 +1,21 @@
 #!/system/bin/sh
-su -c "su -p --setprop ro.build.tags release-keys"
-su -c "su -p --setprop ro.debuggable 0"
-su -c "su -p --setprop ro.secure 1"
+
+# General root hiding
+resetprop ro.debuggable 0
+resetprop ro.secure 1
+resetprop ro.build.type user
+resetprop persist.kernel.su 0
+resetprop ro.kernel.qemu 0
+resetprop ro.adb.secure 1
+resetprop ro.bootmode unknown
+resetprop ro.boot.verifiedbootstate green
+resetprop ro.boot.flash.locked 1
+resetprop ro.boot.vbmeta.device_state locked
+resetprop ro.boot.warranty_bit 0
+resetprop ro.vendor.boot.warranty_bit 0
+
+# Hide KernelSU-specific traces
+resetprop persist.ksu.enabled 0
+resetprop persist.kernel.su 0
+resetprop persist.kernel.su.hide 1
+resetprop sys.oem_unlock_allowed 0
