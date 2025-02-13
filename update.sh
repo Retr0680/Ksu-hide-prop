e3cdb067b8b4ea1d457fb1aef9de6421c854b2be
Retr0 <hrithvikbhardwaj680@gmail.com>
2025-02-13 17:30:56 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 
-# Define GitHub repository details
+# Define the GitHub repository details
 REPO_OWNER="Retr0680"
-REPO_NAME="ksu-hide-prop"
+REPO_NAME="Ksu-hide-prop"
 MODULE_ZIP="ksu-hide-prop.zip"
 UPDATE_URL="https://github.com/$REPO_OWNER/$REPO_NAME/releases/latest/download/$MODULE_ZIP"
 
@@ -11,20 +11,21 @@ TMP_DIR="/sdcard/Download/KernelSU_Update"
 mkdir -p $TMP_DIR
 UPDATE_ZIP="$TMP_DIR/$MODULE_ZIP"
 
-# Download the latest update from GitHub
-echo "[*] Downloading latest update from GitHub..."
+# Check for updates manually via Kernelsu or other methods
+echo "[*] Checking for updates..."
+
+# Download the latest module ZIP from GitHub
 curl -L -o "$UPDATE_ZIP" "$UPDATE_URL"
 
 # Check if the download was successful
 if [ $? -eq 0 ]; then
     echo "[✔] Download successful!"
     
-    # Optionally, you could add a command here to update the module or reboot
-    echo "[*] Updating module..."
-    # Uncomment below line to automatically install the update
+    # You can automatically install the update by extracting the new module
+    # Uncomment the following line if you want to update the module automatically
     # unzip -o "$UPDATE_ZIP" -d "$MODPATH"
-    
-    echo "[*] Please reboot your device to complete the update."
+
+    echo "[*] Update available! Please reboot your device to complete the update."
 else
     echo "[!] Download failed."
     exit 1
