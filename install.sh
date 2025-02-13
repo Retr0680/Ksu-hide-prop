3d6e490cd24cfd954fb9b4739bede611d01caca1
Retr0680 <dedsecretr0680@gmail.com>
2025-02-13 15:15:26 +0530

    added asci logo while installing the module.

--- install.sh
+++ install.sh
@@ -1,2 +1,25 @@
 #!/system/bin/sh
+
+clear  # Clears the screen for a clean look
+
+echo "======================================"
+echo" ██▀███  ▓█████▄▄▄█████▓ ██▀███   ▒█████  "
+echo"▓██ ▒ ██▒▓█   ▀▓  ██▒ ▓▒▓██ ▒ ██▒▒██▒  ██▒"
+echo"▓██ ░▄█ ▒▒███  ▒ ▓██░ ▒░▓██ ░▄█ ▒▒██░  ██▒"
+echo"▒██▀▀█▄  ▒▓█  ▄░ ▓██▓ ░ ▒██▀▀█▄  ▒██   ██░"
+echo"░██▓ ▒██▒░▒████▒ ▒██▒ ░ ░██▓ ▒██▒░ ████▓▒░"
+echo"░ ▒▓ ░▒▓░░░ ▒░ ░ ▒ ░░   ░ ▒▓ ░▒▓░░ ▒░▒░▒░"
+echo"  ░▒ ░ ▒░ ░ ░  ░   ░      ░▒ ░ ▒░  ░ ▒ ▒░"
+echo"  ░░   ░    ░    ░        ░░   ░ ░ ░ ░ ▒ "
+echo"   ░        ░  ░           ░         ░ ░"
+echo "======================================"
+echo "  KernelSU Prop Hide Module Installer"
+echo "======================================"
+sleep 2  # Wait for 2 seconds before proceeding
+
+echo -e "\e[32m[*] Checking for updates...\e[0m"
+
+sh "$MODPATH/update.sh"
+
 chmod +x "$MODPATH/boot-completed.sh"
+sh "$MODPATH/boot-completed.sh"
